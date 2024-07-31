27ac100330f3bc9b4a25f65fe1edabbe84b823de
danilppzz <josedanielmorebolopez@gmail.com>
2024-07-31 14:57:18 +0200

    Create Button.tsx

---
+++ components/Button.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: "default" | "icon";
+  variant?: "default" | "outline" | "ghost";
+  children: React.ReactNode;
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
+}
+
+const Button: React.FC<ButtonProps> = ({ children, size = "default", variant = "default", prefix = null, suffix = null, ...rest }) => {
+  const baseClasses = "h-[36px] rounded-md text-[14px] font-sans font-semibold flex items-center transition-all duration-300 ease-in-out active:scale-95";
+  const sizeClasses = size === "default" ? "px-4 py-auto" : "w-[36px] justify-center";
+  
+  let variantClasses;
+  switch (variant) {
+    case "outline":
+      variantClasses = "bg-transparent border border-white/10 text-white/80";
+      break;
+    case "ghost":
+      variantClasses = "bg-transparent text-white/80 hover:bg-[#2E2E33]";
+      break;
+    case "default":
+    default:
+      variantClasses = "bg-[#2E2E33] bg-opacity-75 hover:bg-opacity-100 text-white/80";
+      break;
+  }
+
+  return (
+    <button
+      {...rest}
+      className={`${baseClasses} ${sizeClasses} ${variantClasses}`}
+    >
+      {prefix && <span className="mr-2">{prefix}</span>}
+      {children}
+      {suffix && <span className="ml-2">{suffix}</span>}
+    </button>
+  );
+};
+
+export default Button;
